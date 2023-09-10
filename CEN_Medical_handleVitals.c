edbe3bd037a8faa6d162ecf57d9600097c35c0ed
drPanda1893 <142361901+drPanda1893@users.noreply.github.com>
2023-09-10 18:05:37 +0200

    Implements Vitals can be called by Statemachine

--- CEN_Medical_handleVitals.c
+++ CEN_Medical_handleVitals.c
@@ -1,4 +1,4 @@
-enum CENMedicalECharacterBloodState
+enum CEN_Medical_ECharacterBloodState
 {
 BLOOD_VOLUME_CLASS_1_HEMORRHAGE,
 BLOOD_VOLUME_CLASS_2_HEMORRHAGE,
@@ -11,13 +11,13 @@ modded class SCR_ChimeraCharacter
 {
     protected float m_fCEN_Medical_lastUpdateTime = 0;
 
-    //------------------------------------------------------------------------------------------------
+ //------------------------------------------------------------------------------------------------
     float CEN_Medical_getLastUpdateTime()
     {
      return m_fCEN_Medical_lastUpdateTime;
     }
 
-    //------------------------------------------------------------------------------------------------
+ //------------------------------------------------------------------------------------------------
     void CEN_Medical_updateLastUpdateTime()
     {
      m_fCEN_Medical_lastUpdateTime = GetGame().GetWorld().GetWorldTime();
