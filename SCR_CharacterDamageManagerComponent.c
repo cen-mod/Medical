e902a44ec47f60bbeab2498a0abe59ccf5849e5a
drPanda1893 <142361901+drPanda1893@users.noreply.github.com>
2023-09-25 21:28:49 +0200

    Reads Bloodvolume and gets new hemmorhage classes

---
+++ SCR_CharacterDamageManagerComponent.c
@@ -0,0 +1,56 @@
+enum CEN_Medical_ECharacterBloodState
+{
+	BLOOD_VOLUME_CLASS_1_HEMORRHAGE,
+	BLOOD_VOLUME_CLASS_2_HEMORRHAGE,
+	BLOOD_VOLUME_CLASS_3_HEMORRHAGE,
+	BLOOD_VOLUME_CLASS_4_HEMORRHAGE,
+	BLOOD_VOLUME_FATAL
+};
+
+//------------------------------------------------------------------------------------------------
+ modded class SCR_CharacterDamageManagerComponent  : ScriptedDamageManagerComponent
+{
+	protected float m_fBloodVolume4;
+	protected float m_fBloodVolume3;
+	protected float m_fBloodVolume2;
+	protected float m_fBloodVolume1;
+	protected float m_fBloodVolumeFatal;
+	//------------------------------------------------------------------------------------------------
+	float getBloodVolume(SCR_ChimeraCharacter unit )
+	{	
+		SCR_CharacterDamageManagerComponent damageManagerComponent = SCR_CharacterDamageManagerComponent.Cast(unit.FindComponent(SCR_CharacterDamageManagerComponent));
+		SCR_CharacterBloodHitZone bloodvolume = SCR_CharacterBloodHitZone.Cast( damageManagerComponent.GetHitZoneByName("Blood"));
+		float m_fbloodVolume = bloodvolume.GetHealth();
+		return m_fbloodVolume;
+	}
+	//------------------------------------------------------------------------------------------------
+	int GetHemorrhageClass(SCR_ChimeraCharacter unit)
+	{
+		m_fBloodVolume1 = m_pBloodHitZone.GetDamageStateThreshold(CEN_Medical_ECharacterBloodState.BLOOD_VOLUME_CLASS_1_HEMORRHAGE);
+		m_fBloodVolume2 = m_pBloodHitZone.GetDamageStateThreshold(CEN_Medical_ECharacterBloodState.BLOOD_VOLUME_CLASS_2_HEMORRHAGE);
+		m_fBloodVolume3 = m_pBloodHitZone.GetDamageStateThreshold(CEN_Medical_ECharacterBloodState.BLOOD_VOLUME_CLASS_3_HEMORRHAGE);
+		m_fBloodVolume4 = m_pBloodHitZone.GetDamageStateThreshold(CEN_Medical_ECharacterBloodState.BLOOD_VOLUME_CLASS_4_HEMORRHAGE);
+		
+		if(getBloodVolume(unit) < m_fBloodVolume1)
+		{
+			return CEN_Medical_ECharacterBloodState.BLOOD_VOLUME_CLASS_1_HEMORRHAGE;
+		}
+		else if(getBloodVolume(unit) < m_fBloodVolume2 )
+			{
+				return CEN_Medical_ECharacterBloodState.BLOOD_VOLUME_CLASS_2_HEMORRHAGE;
+			}
+		else if(getBloodVolume(unit) < m_fBloodVolume3 )
+			{
+				return CEN_Medical_ECharacterBloodState.BLOOD_VOLUME_CLASS_3_HEMORRHAGE;
+			}
+		else if(getBloodVolume(unit) < m_fBloodVolume4 )
+			{
+				return CEN_Medical_ECharacterBloodState.BLOOD_VOLUME_CLASS_4_HEMORRHAGE;
+			}
+		else 
+			{
+				return -1;
+			}
+	}
+	
+};
